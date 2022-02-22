cf1eea3662990dcfeaee44e4c643001d19b88328
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-02-22 17:27:48 +0900

    startcraft

--- method.py
+++ method.py
@@ -4,10 +4,17 @@ class Unit:
         self.name = name # 멤버 변수(클래스 내에서 정의된 변수)
         self.hp = hp
         self.speed = speed
-
+        print("{0} 유닛이 생성되었습니다.".format(name))
     def move(self, location):
         print("[지상 유닛 이동]")
         print("{0} : {1} 방향으로 이동합니다. [속도 : {2}]".format(self.name, location, self.speed))
+    
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} 유닛은 파괴되었습니다.".format(self.name))
 
 # 공격 유닛
 class AttackUnit(Unit): # 상속
@@ -18,12 +25,36 @@ class AttackUnit(Unit): # 상속
     def attack(self, location):
         print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
 
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} 유닛은 파괴되었습니다.".format(self.name))
+class Marine(AttackUnit):
+    def __init__(self):
+        AttackUnit.__init__(self, "마린", 40, 1, 5)
+
+    def stimpack(self):
+        if self.hp > 10:
+            self.hp -= 10
+            print("{0} : 스팀팩을 사용합니다. [hp 10 감소]".format(self.name))
+        else:
+            print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다.".format(self.name))
+        
+class Tank(AttackUnit):
+    seige_developed = False # 시즈모드 개발여부
+    
+    def __init__(self):
+        AttackUnit.__init__(self, "탱크", 150, 1, 35)
+        self.seige_mode = False
+
+    def set_seige_mode(self):
+        if Tank.seige_developed == False:
+            return
+        
+        if self.seige_mode == False:
+            print("{0} : 시즈모드 전환합니다.".format(self.name))
+            self.damage *= 2
+            self.seige_mode = True
+        else:
+            print("{0} : 시즈모드를 해체합니다.".format(self.name))
+            self.damage /= 2
+            self.seige_mode = False
 
 # 날 수 있는 기능을 가진 클래스
 class Flyable:
@@ -42,12 +73,24 @@ class FlyableAttackUnit(AttackUnit, Flyable):
         print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
+class Wraith(FlyableAttackUnit):
+    def __init__(self):
+        FlyableAttackUnit.__init__(self, "레이스", 80, 20, 5)
+        self.clocked = False # 클로킹 모드(해제상태)
+
+    def clocking(self):
+        if self.clocked == True:
+            print("{0} : 클로킹 모드를 해제합니다.".format(self.name))
+            self.clocked = False
+        else:
+            print("{0} : 클로킹 모드를 설정합니다.".format(self.name))
+            self.clocked = True
 # 건물
-class BuildingUnit(Unit):
-    def __init__(self, name, hp, location):
-        # Unit.__init__(self, name, hp, 0)
-        super().__init__(name, hp, 0)
-        self.location = location
+# class BuildingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#         # Unit.__init__(self, name, hp, 0)
+#         super().__init__(name, hp, 0)
+#         self.location = location
         # pass # 아무것도 안하고 일단 넘어감
 
 
