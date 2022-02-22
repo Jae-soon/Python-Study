134f3c7af1db9bfeccddbd22ce6bf02dfb1c97e1
Jae-Soon Kwon <rnjswotns12@naver.com>
2022-02-22 17:34:37 +0900

    startcraft attack

--- method.py
+++ method.py
@@ -1,3 +1,5 @@
+from random import *
+
 # 일반 유닛
 class Unit:
     def __init__(self, name, hp, speed):
@@ -5,6 +7,7 @@ class Unit:
         self.hp = hp
         self.speed = speed
         print("{0} 유닛이 생성되었습니다.".format(name))
+
     def move(self, location):
         print("[지상 유닛 이동]")
         print("{0} : {1} 방향으로 이동합니다. [속도 : {2}]".format(self.name, location, self.speed))
@@ -85,6 +88,65 @@ class Wraith(FlyableAttackUnit):
         else:
             print("{0} : 클로킹 모드를 설정합니다.".format(self.name))
             self.clocked = True
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    print("Player : gg")
+    print("[Player] 님이 퇴장하셨습니다.")
+
+# 게임 시작
+game_start()
+
+# 유닛 생성
+m1 = Marine()
+m2 = Marine()
+m3 = Marine()
+
+t1 = Tank()
+t2 = Tank()
+
+w1 = Wraith()
+
+# 유닛 일괄 관리
+attack_units = []
+attack_units.append(m1)
+attack_units.append(m2)
+attack_units.append(m3)
+attack_units.append(t1)
+attack_units.append(t2)
+attack_units.append(w1)
+
+# 전군 이동
+for unit in attack_units:
+    unit.move("1시")
+
+# 탱크 시즈모드 개발
+Tank.seige_developed = True
+print("[알림] 탱크 시즈 모드 개발이 완료되었습니다.")
+
+# 공격모드 준비 (마린 : 스팀팩 사용, 탱크 : 시즈모드, 레이스 : 클로킹)
+for unit in attack_units:
+    if isinstance(unit, Marine):
+        unit.stimpack
+    elif isinstance(unit, Tank):
+        unit.set_seige_mode()
+    elif isinstance(unit, Wraith):
+        unit.clocking()
+
+# 전군 공격
+for unit in attack_units:
+    unit.attack("1시")
+
+# 전군 피해
+for unit in attack_units:
+    unit.damaged(randint(5, 21)) # (5~20) 까지 랜덤으로 공격받음
+
+# 게임 종료
+game_over()
+
+
 # 건물
 # class BuildingUnit(Unit):
 #     def __init__(self, name, hp, location):
